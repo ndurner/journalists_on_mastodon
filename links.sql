01a212dbfff3b467c79eae2ae39c3dfded9ff744
Nils Durner <n.durner@namirial.com>
2022-11-21 09:20:08 +0100

    cleanup

--- links.sql
+++ links.sql
@@ -1,6 +1,12 @@
 -- fix data
 update journalists set Mastodon = trim(Mastodon);
 update journalists set Mastodon = '@taylorlorenz@mastodon.social' where Mastodon = '@taylorlorenz.mastodon.social';
+update journalists set Mastodon = '@shelley_wine@mastodon.social' where Mastodon = '@shelley_wine';
+update journalists set Mastodon = '@jilleduffy@mastodon.social' where Mastodon = '@jilleduffy@mastondon.social';
+update journalists set Mastodon = '@merryarty@mastodon.green' where Mastodon = '@merryarty';
+delete from journalists where Timestamp = '11/6/2022 8:27:53'; -- not a journalist
+delete from journalists where Mastodon = '@jacobytekel@sciences.social'; -- not a journalist
+delete from journalists where Mastodon = '@elizgrenier@c.im'; -- non existant
 
 -- Mastodon handles given as full link
 update journalists set Link = Mastodon where Mastodon like 'http%';
